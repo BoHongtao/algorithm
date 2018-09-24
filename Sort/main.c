9ed41d393032842adb399691c5d7636a0aa7b0f2
BoHongtao <BoHongtao@yeah.net>
2018-09-24 14:26:03 +0800

    QuickSort

--- Sort/main.c
+++ Sort/main.c
@@ -49,10 +49,13 @@ void InsertSort(int *a,int n)
 {
     int i,j;
     int tmp;
+    //第一个元素是有序的，从第二个元素开始排序
     for(i=1;i<n;++i)
     {
         tmp = a[i];
+        //j是有序元素的最大下表
         j = i -1;
+        //为将要插入的元素腾位置
         while(j>=0 && tmp<a[j])
         {
             a[j+1] = a[j];
@@ -60,6 +63,40 @@ void InsertSort(int *a,int n)
         }
         a[j+1] = tmp;
     }
+}
+
+//快速排序，对a数组的l-r段内的元素进行排序
+void QuickSort(int *a,int L,int R)
+{
+    int tmp;
+    int i = L;
+    int j = R;
+    if (L < R)
+    {
+        //选择第一个元素为枢轴
+        tmp = a[L];
+        while(i!=j)
+        {
+            while(j>i && a[j]>tmp)
+                --j;
+            if(i<j)
+            {
+                a[i] = a[j];
+                ++i;
+            }
+            while(i<j && a[i]<tmp)
+                ++i;
+            if(i<j)
+            {
+                a[j] = a[i];
+                --j;
+            }
+        }
+        a[i] = tmp;
+        QuickSort(a,L,i-1);
+        QuickSort(a,i+1,R);
+    }
+
 }
 
 
@@ -70,7 +107,8 @@ int main()
     int length = sizeof(arr) / sizeof(arr[0]);
     //SelectSort(arr,length);
     //BubbleSort(arr,length);
-    InsertSort(arr,length);
+    //InsertSort(arr,length);
+    QuickSort(arr,0,9);
     for(int i=0;i<length;++i)
     {
         printf("%d ",arr[i]);
